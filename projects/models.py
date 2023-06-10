744086493e7df54ab4759476ddfae9a4c9c91853
abdelrahman elkady <112387841+Abjudge@users.noreply.github.com>
2023-06-10 22:14:42 +0300

    Revert "created in project model classes( project-tag-category-photo)"

--- projects/models.py
+++ projects/models.py
@@ -1,26 +1,3 @@
 from django.db import models
 
-class Tag(models.Model):
-    name = models.CharField(max_length=50)
-
-
-
-class Category(models.Model):
-    name = models.CharField(max_length=50)
-
-class Project(models.Model):
-    title = models.CharField(max_length=100)
-    details = models.TextField()
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    target = models.FloatField()
-    tags = models.ManyToManyField(Tag)
-    pub_date = models.DateTimeField()
-    start_date = models.DateTimeField()
-    end_date = models.DateTimeField()
-
-
-
-class Photo(models.Model):
-    project = models.ForeignKey(Project, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='photos/')
-
+# Create your models here.
