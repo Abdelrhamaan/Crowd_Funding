8716083a600e8034afeff779388c542b06d31f52
abjudge <abdelrahmanelkady0@gmail.com>
2023-06-10 19:21:37 +0300

    created in project model classes( project-tag-category-photo)

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    details = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    target = models.FloatField()
+    tags = models.ManyToManyField(Tag)
+    pub_date = models.DateTimeField()
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+
+
+
+class Photo(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='photos/')
+
