0a003b6ecb2b60ae4f1e7276ae385f3f3f3fa7ca
Abdelrhamaan <abdelrhmanmamdouh76@gmail.com>
2023-06-10 16:13:47 +0300

    Create Project Files

---
+++ myaccount/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def Login(request):
+    pass
+
+
+def Logout(request):
+    pass
+
+
+def Registration(request):
+    pass
+
+
+def user_list(request):
+    pass
